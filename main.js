cb8901794e7415c1053baee7433e114c23de90b3
Yun Jia <yunjia@me.com>
2023-07-31 16:50:26 -0700

    chore: Initial attempt to run a ROS2 publisher node.

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
+const rclnodejs = require('rclnodejs');
+rclnodejs.init().then(() => {
+  const node = new rclnodejs.Node('publisher_example_node');
+  const publisher = node.createPublisher('std_msgs/msg/String', 'topic');
+  publisher.publish(`Hello ROS 2 from rclnodejs`);
+  node.spin(3000);
+});
 
+// app.commandLine.appendSwitch('high-dpi-support', 1)
+// app.commandLine.appendSwitch('force-device-scale-factor', 1.5)
 function createWindow () {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false
     }
   })
 
